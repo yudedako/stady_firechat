024ac8d38feee3878979b772e7cda3effc592b82
yudedako <pluto.club@gmail.com>
2018-08-14 17:56:30 +0900

    authとchatの処理を実装

--- src/App.vue
+++ src/App.vue
@@ -45,37 +45,71 @@ import Nl2br from 'vue-nl2br'
 
 export default {
   components: { Nl2br },
-  date() {
-
+  data() {
+    return {
+      user: {},  // ユーザー情報
+      chat: [],  // 取得したメッセージを入れる配列
+      input: ''  // 入力したメッセージ
+    }
   },
 
   created() {
-
+    firebase.auth().onAuthStateChanged(user => {
+      this.user = user ? user : {}
+      const ref_message = firebase.database().ref('message')
+      if (user) {
+        this.chat = []
+        // message に変更があったときのハンドラを登録
+        ref_message.limitToLast(10).on('child_added', this.childAdded)
+      } else {
+        // message に変更があったときのハンドラを解除
+        ref_message.limitToLast(10).off('child_added', this.childAdded)
+      }
+    })
   },
 
   methods: {
     // ログイン処理
     doLogin() {
-
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
     },
 
     // ログアウト処理
     doLogout() {
-
+      firebase.auth().signOut()
     },
 
     // スクロール位置を一番下に移動
     scrollBottom() {
-
+      this.$nextTick(() => {
+        window.scrollTo(0, document.body.clientHeight)
+      })
     },
 
     // 受け取ったメッセージをchatに追加
     // データベースに新しい要素が追加されると随時呼び出される
     childAdded(snap) {
-
+      const message = snap.val()
+      this.chat.push({
+        key: snap.key,
+        name: message.name,
+        image: message.image,
+        message: message.message
+      })
+      this.scrollBottom()
     },
     doSend() {
-
+      if (this.user.uid && this.input.length) {
+        // firebase にメッセージを追加
+        firebase.database().ref('message').push({
+          message: this.input,
+          name: this.user.displayName,
+          image: this.user.photoURL
+        }, () => {
+          this.input = ''  // フォームを空にする
+        })
+      }
     }
   }
 }
